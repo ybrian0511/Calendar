a8f811f16a0024a7d84f5881dc98439a99cab4ff
ybrian0511 <101641371+ybrian0511@users.noreply.github.com>
2022-04-05 02:30:25 +0900

    Initial commit

--- app/src/main/java/com/hansung/android/calendar/MonthViewActivity.java
+++ app/src/main/java/com/hansung/android/calendar/MonthViewActivity.java
@@ -32,6 +32,48 @@ public class MonthViewActivity extends AppCompatActivity {
         buttonClick();
     }
 
+    public ArrayList<String> CalendarDay(Calendar date){ //
+        ArrayList <String> days_in_month = new ArrayList();
+        Calendar cal = date;
+        cal.set(Calendar.DATE,1); // 날짜 지정
+
+        int day_of_week = cal.get(Calendar.DAY_OF_WEEK)-1; // 현재 요일
+        int num_of_day = cal.getActualMaximum(Calendar.DATE); // 현재 월의 날짜의 최대 수 (1월:31일 2월:28일..)
+
+        for (int i = 1; i <= 42; i++) { // 6행 7열 >> 42개
+            if(i<= day_of_week || i> (num_of_day + day_of_week)) // i가 요일보다 작거나 i가
+                days_in_month.add(""); // 공백 추가
+            else
+                days_in_month.add(String.valueOf(i-day_of_week));
+        }
+        return days_in_month;
+    }
+
+    private String StringDay(Calendar date){
+        SimpleDateFormat today = new SimpleDateFormat("yyyy년 MM월"); // 날짜 포맷 지정
+        return today.format(date.getTime()); // 현재 날짜
+    }
+
+    private void CalendarView() { // 캘린더 뷰 함수
+        yearMonthTitle.setText(StringDay(cal)); // 현재 년도와 월 저장
+        ArrayList<String> days = CalendarDay(cal); // 데이터 원본 준비
+
+        GridAdapter adapter = new GridAdapter(this,R.layout.day, days);  // 어댑터 생성
+
+        GridView GridView = (GridView) findViewById(R.id.gridview); // 어댑터 연결
+        GridView.setAdapter(adapter);
+
+        GridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View vClicked, int position, long id) {
+                String date = adapter.getItem(position);
+                int Calendar_Month = cal.get(Calendar.MONTH) + 1; // +1을 하는 이유는 1월이 0이기 때문이다.
+                int Calendar_Year = cal.get(Calendar.YEAR);
+                Toast.makeText(MonthViewActivity.this, Calendar_Year+"."+Calendar_Month+"."+date ,
+                        Toast.LENGTH_SHORT).show(); // 날짜 클릭 시 년.월.일 메시지 출력
+            }
+        });
+    }
+
     private void buttonClick() { // 버튼 클릭 함수
         Button leftButton = (Button)findViewById(R.id.left_button);
         leftButton.setOnClickListener(new View.OnClickListener() {
@@ -53,53 +95,12 @@ public class MonthViewActivity extends AppCompatActivity {
         });
     }
 
-    private String StringDay(Calendar date){
-        SimpleDateFormat today = new SimpleDateFormat("yyyy년 MM월"); // 날짜 포맷 지정
-        return today.format(date.getTime()); // 현재 날짜
-    }
-
     private void reset(){ // 초기화 함수
         Intent intent = new Intent(MonthViewActivity.this, MonthViewActivity.class); // 현재 액티비티에서 현재 액티비티 호출
         intent.putExtra("Date", cal); //
         startActivity(intent); // 인텐트 객체 전달
         finish(); // 현재의 액티비티를 종료
     }
-    private void CalendarView() { // 캘린더 뷰 함수
-        yearMonthTitle.setText(StringDay(cal)); // 현재 년도와 월 저장
-        ArrayList<String> days = CalendarDay(cal); // 데이터 원본 준비
-
-        GridAdapter adapter = new GridAdapter(this,R.layout.day, days);  // 어댑터 생성
-
-        GridView GridView = (GridView) findViewById(R.id.gridview); // 어댑터 연결
-        GridView.setAdapter(adapter);
-
-        GridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View vClicked, int position, long id) {
-                String date = adapter.getItem(position);
-                int Calendar_Month = cal.get(Calendar.MONTH) + 1; // +1을 하는 이유는 1월이 0이기 때문이다.
-                int Calendar_Year = cal.get(Calendar.YEAR);
-                Toast.makeText(MonthViewActivity.this, Calendar_Year+"."+Calendar_Month+"."+date ,
-                        Toast.LENGTH_SHORT).show(); // 날짜 클릭 시 년.월.일 메시지 출력
-            }
-        });
-    }
-
-
-    public ArrayList<String> CalendarDay(Calendar date){ //
-        ArrayList <String> days_in_month = new ArrayList();
-        Calendar cal = date;
-        cal.set(Calendar.DATE,1); // 날짜 지정
-
-        int day_of_week = cal.get(Calendar.DAY_OF_WEEK)-1; // 현재 요일
-        int num_of_day = cal.getActualMaximum(Calendar.DATE); // 현재 월의 날짜의 최대 수 (1월:31일 2월:28일..)
-
-        for (int i = 1; i <= 42; i++) { // 6행 7열 >> 42개
-            if(i<= day_of_week || i> (num_of_day + day_of_week)) // i가 요일보다 작거나 i가
-                days_in_month.add(""); // 공백 추가
-            else
-                days_in_month.add(String.valueOf(i-day_of_week));
-        }
-        return days_in_month;
-    }
+    
 }
 
